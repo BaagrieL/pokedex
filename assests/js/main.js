7b0baf7e2b245469e7a2692ecf0a5536f7babbb7
Gabriel Azevedo <113298016+BaagrieL@users.noreply.github.com>
2024-06-14 21:17:38 -0300

    Mudei as cores dos cards de pokemon

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,22 +1,21 @@
 const pokeContainer = document.querySelector ('.pokemons');
 const pokemonCount = 51;
 const colors = {
-    fire: '#FDDFDF',
-    grass: '#DEFDE0',
-    electric: '#FCF7DE',
-    water: '#DEF3FD',
-    ground: '#f4e7da',
-    rock: '#d5d5d4',
-    fairy: '#fceaff',
-    poison: '#98d7a5',
-    bug: '#f8d5a3',
-    dragon: '#97b3e6',
-    psychic: '#eaeda1',
-    flying: '#F5F5F5',
-    fighting: '#E6E0D4',
-    normal: '#d9d9d9'
-}
-
+    fire: '#FF7F7F',
+    grass: '#9BCD9B',   
+    electric: '#FFD700',  
+    water: '#6495ED',     
+    ground: '#CD853F',
+    rock: '#A9A9A9',
+    fairy: '#FFB5C5',    
+    poison: '#BA55D3',    
+    bug: '#32CD32',       
+    dragon: '#8470FF',    
+    psychic: '#FF69B4',   
+    flying: '#87CEEB',   
+    fighting: '#CD5C5C',  
+    normal: '#D3D3D3'     
+};
 const mainTypes = Object.keys(colors);
 
 const fetchPokemons = async () => {
